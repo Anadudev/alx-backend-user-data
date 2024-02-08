a360bde7bada565b9099bf6d305f8d426b336680
AnaduCG <godzionwin@gmail.com>
2024-02-08 23:08:08 +0100

    created encrypt_password.py file

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" Encrypting passwords """
+
+
+def hash_password(password: str):
+    """_summary_
+
+    Args:
+        password (str): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password.encode(), salt)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """_summary_
+
+    Args:
+        hashed_password (bytes): _description_
+        password (str): _description_
+
+    Returns:
+        bool: _description_
+    """
+    if bcrypt.checkpw(password, hashed_password):
+        return True
+    return False
