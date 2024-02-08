3bdf2bf83c73b94b192db4d5c4aecb3573b6e174
AnaduCG <godzionwin@gmail.com>
2024-02-08 11:11:31 +0100

    corrected typo and added annotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,14 +5,15 @@ import logging
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
+
 def filter_datum(fields, redaction, message, separator):
     for i in fields:
         message = re.sub(f"{i}=.*?{separator}", f"{i}={redaction}{separator}", message)
     return message
 
+
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """Redacting Formatter class"""
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
@@ -23,15 +24,26 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-         record.msg = filter_datum(self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
-         return super(RedactingFormatter,self).format(record)
-def get_logger():
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR
+        )
+        return super(RedactingFormatter, self).format(record)
+
+
+def get_logger() -> logging.Logger:
+    """_summary_
+
+    Returns:
+        logging.Logger: _description_
+    """
     logger = logging.getLogger("user_data")
-    logegr.setLevel(logging.INFO)
+    logger.setLevel(logging.INFO)
     logger.propagate = False
-    handler = logging.StremHandler()
+    handler = logging.StreamHandler()
     formatter = RedactingFormatter(list(PII_FIELDS))
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     return logger
-# if __name__="__main__":
+
+
+# if __name__=="__main__":
