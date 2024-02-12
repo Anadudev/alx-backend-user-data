1fd7159bc7068a23763c2e8c4c4e97a919050252
AnaduCG <godzionwin@gmail.com>
2024-02-12 10:15:40 +0100

    setting up flask application

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -14,6 +14,16 @@ def status() -> str:
     return jsonify({"status": "OK"})
 
 
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def Unauthorized() -> str:
+    """ GET /api/v1/Unauthorized
+    Return:
+      - the Unauthorized of the API
+    """
+    # return jsonify({"status": "OK"})
+    abort(401)
+
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
