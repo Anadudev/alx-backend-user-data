6c080dac87ea509159cb2266f3f1890aca4d1571
AnaduCG <godzionwin@gmail.com>
2024-02-13 10:10:24 +0100

    pycodetyling and functioin docstring

---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""Auth class"""
+from flask import Flask
+from typing import List, TypeVar
+import re
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """_summary_
+
+        Args:
+            path (str): _description_
+            excluded_paths (List[str]): _description_
+
+        Returns:
+            bool: _description_
+        """
+        pattern = r"/api/v1/status(/?)"
+        if path is None:
+            return True
+        if re.match(pattern, path):
+            for paths in excluded_paths:
+                if re.match(pattern, paths):
+                    return False
+        if path not in excluded_paths:
+            return True
+        if not excluded_paths:
+            return True
+        # for string in excluded_paths:
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """_summary_
+
+        Args:
+            request (_type_, optional): _description_. Defaults to None.
+
+        Returns:
+            str: _description_
+        """
+        if request is None:
+            return None
+        if "Authorization" not in request.headers.keys():
+            return None
+        return request.headers["Authorization"]
+
+    def current_user(self, request=None) -> TypeVar("User"):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        return None
--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -5,18 +5,18 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route("/status", methods=["GET"], strict_slashes=False)
 def status() -> str:
-    """ GET /api/v1/status
+    """GET /api/v1/status
     Return:
       - the status of the API
     """
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+@app_views.route("/unauthorized", methods=["GET"], strict_slashes=False)
 def Unauthorized() -> str:
-    """ GET /api/v1/Unauthorized
+    """GET /api/v1/Unauthorized
     Return:
       - the Unauthorized of the API
     """
@@ -24,22 +24,24 @@ def Unauthorized() -> str:
     abort(401)
 
 
-@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+@app_views.route("/forbidden", methods=["GET"], strict_slashes=False)
 def Forbidden() -> str:
-    """ GET /api/v1/Unauthorized
+    """GET /api/v1/Unauthorized
     Return:
       - the Unauthorized of the API
     """
     # return jsonify({"status": "OK"})
     abort(403)
 
-@app_views.route('/stats/', strict_slashes=False)
+
+@app_views.route("/stats/", strict_slashes=False)
 def stats() -> str:
-    """ GET /api/v1/stats
+    """GET /api/v1/stats
     Return:
       - the number of each objects
     """
     from models.user import User
+
     stats = {}
-    stats['users'] = User.count()
+    stats["users"] = User.count()
     return jsonify(stats)
