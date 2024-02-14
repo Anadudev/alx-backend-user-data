491578785a657ac5bac0d21031d27ac808447084
AnaduCG <godzionwin@gmail.com>
2024-02-14 10:34:34 +0100

    Setup: setting up required files

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""Auth class"""
+from flask import request
+from typing import List, TypeVar
+import re
+
+
+class Auth:
+    """Class that implements user authentication"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """method that validates and authenticates a url
+
+        Args:
+            path (str): url path check
+            excluded_paths (List[str]): list of excluded url path
+
+        Returns:
+            bool: _description_
+        """
+
+        pattern = r"/api/v1/status(/?)"
+        if not(path and excluded_paths):
+            return True
+        if path in excluded_paths:
+            return False
+        if re.match(pattern, path):
+            for paths in excluded_paths:
+                if re.match(pattern, paths):
+                    return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """Method that implements a request header
+
+        Args:
+            request (_type_, optional): flask request object. Defaults to None.
+
+        Returns:
+            str: _description_
+        """
+        if request is None:
+            return None
+        if "Authorization" not in request.headers.keys():
+            return None
+        return request.headers["Authorization"]
+
+    def current_user(self, request=None) -> TypeVar("User"):
+        """Method that gets the current user
+
+        Returns:
+            _type_: _description_
+        """
+        return None
