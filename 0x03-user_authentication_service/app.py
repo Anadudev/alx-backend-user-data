a7628b8b66e4b673ded128617af7a519228f7896
Anadudev <anadudev@gmail.com>
2024-02-21 12:41:20 +0000

    Update: implementing user_registration route

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""a simple flask application"""
+from flask import Flask, jsonify, request
+from auth import Auth
+
+
+app = Flask(__name__)
+AUTH = Auth()
+
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({"message": "Bienvenue"})
+
+@app.route("/users", methods=["POST"])
+def register_users():
+    data = request.form
+    email = data.("email")
+    password = data."(password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": "", "message": "user created"})
+    except Exception:
+        return jsonify({"message": "email already registered"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
