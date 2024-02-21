15484194b4ba4e9fae57ec5e9b32aa23c1e879b8
Anadudev <anadudev@gmail.com>
2024-02-21 09:29:58 +0000

    Update: added update_user docstring

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -51,6 +51,7 @@ class DB:
         return my_user
 
     def update_user(self, user_id: int, **kwargs: Dict) -> None:
+        """ a method that updates a user attributes """
         user = self.find_user_by(id=user_id)
         for key, value in kwargs.items():
             if not hasattr(User, key):
