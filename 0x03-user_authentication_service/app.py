4ebaf581cebf2d890bcc1ace8e87bf1034b34c70
Anadudev <anadudev@gmail.com>
2024-02-22 14:43:58 +0100

    Update: upser profile view

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -53,5 +53,17 @@ def logout():
     abort(403)
 
 
+@app.route("/profile", methods=["GET"])
+def home():
+    """application home route"""
+    session_id = request.cookies.get("session_id")
+    try:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            return jsonify({"email": f"{user.email}"})
+    except Exception:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
