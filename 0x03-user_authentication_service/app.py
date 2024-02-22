f29780ce3abdded70289a4636150f25c9b75aabd
Anadudev <anadudev@gmail.com>
2024-02-22 13:58:20 +0100

    Update: reimplementing custom destry_session

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """a simple flask application"""
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
